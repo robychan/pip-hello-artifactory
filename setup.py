95cb3268523a0f58e5fa3c25de8599688804b678
Roby George <roby.george@blrjpt2985d.ad.infosys.com>
2017-04-06 16:01:42 +0530

    Adding pip files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    	name='pip-hello-artifactory',    # This is the name of my PyPI-package.
+    	version='0.1',                          # Update the version number for new releases
+    	scripts=['pip-hello-artifactory'],                  # The name of your scipt, and also the command you'll be using for calling it
+	url='https://github.com/robychan/pip-hello-artifactory.git',
+	author='Roby George',
+	author_email='roby.george@hotmail.com'
+)
